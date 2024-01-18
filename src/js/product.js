8fec1cda6fffc33e2250f90277479de811a5fb82
James Green <github.byui.school@ckplace.org>
2024-01-18 17:21:10 -0500

    fix broken cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,17 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import { findProductById } from './productData.mjs';
 
 function addProductToCart(product) {
+  // get current items that are in the cart.
+  let cartItems = getLocalStorage('so-cart');
+
+  //If cart is empty, we may have an issue so we need to create the array
+  if (!cartItems){
+    cartItems = [];
+  }
+
+  // add the item into the cart and then to cart Items as before
+  cartItems.push(product);
   setLocalStorage('so-cart', product);
 }
 // add to cart button event handler
