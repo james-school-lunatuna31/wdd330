21ae9e0ec7ff5e53296724b66f7d2c491fa5ff8b
Lindsey Goode <ljgoode2012@gmail.com>
2024-01-27 21:33:31 -0500

    Finished stretch goal functions in ProductDetails and ran lint/formatting

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,25 +1,24 @@
 import { findProductById } from './productData.mjs';
+import { setLocalStorage } from './utils.mjs';
 
-
-export default function productDetails(productId) {
-
+export default async function productDetails(productId) {
+  product = await findProductById(productId);
+  renderProductDetails(product);
+ document.getElementById('addToCart').addEventListener('click', addProductToCart(product));
 }
 
 export function addProductToCart(product) {
-
+  setLocalStorage("so-cart", product);
 }
 
-export function renderProductDetails() {
-
+export function renderProductDetails(product) {
+  document.querySelector('#productName').innerText = product.Brand.Name;
+  document.querySelector('#productNameWithoutBrand').innerText =
+    product.NameWithoutBrand;
+  document.querySelector('#productImage').src = product.Image;
+  document.querySelector('#productImage').alt = product.Name;
+  document.querySelector('#productFinalPrice').innerText = product.FinalPrice;
+  document.querySelector('#productColorName').innerText = product.Colors[0].ColorName;
+  document.querySelector('#productDescriptionHtmlSimple').innerHTML = product.DescriptionHtmlSimple;
+  document.querySelector('#addToCart').dataset.id = product.Id;
 }
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
