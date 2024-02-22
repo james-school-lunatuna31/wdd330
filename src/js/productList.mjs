6866b0e77ff76154121f4638c71768f6fedec369
Lindsey Goode <ljgoode2012@gmail.com>
2024-02-21 21:49:22 -0500

    fixed bugs in links to product details. finished title and images stretch challengs

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,20 +1,23 @@
 import { getData } from "./productData.mjs";
-import { renderListWithTemplate } from "./utils.mjs";
+import { getParam, renderListWithTemplate } from "./utils.mjs";
+const baseURL = import.meta.env.VITE_SERVER_URL;
 
 export default async function productList(category, selector) {
   let data = (await getData(category)).filter(
     (product) => product.Id !== "880RT" && product.Id !== "989CG"
   );
   let ele = document.querySelector(selector);
+  let title = document.querySelector('.title');
+  title.innerText = `Top Products: ${category}`
   renderListWithTemplate(productCardTemplate, ele, data);
 }
 
 function productCardTemplate(product) {
   const baseUrl = window.location.origin;
   return `<li class="product-card">
-    <a href="product_pages/index.html?product=${product.Id}">
+    <a href="${baseUrl}/product_pages/index.html?product=${product.Id}">
       <img
-        src=${product.Image}
+        src=${product.Images.PrimaryMedium}
         alt=${product.Name}
       />
       <h3 class="card__brand">${product.Brand.Name}</h3>
@@ -22,4 +25,30 @@ function productCardTemplate(product) {
       <p class="product-card__price">${product.FinalPrice}</p></a
     >
   </li>`;
+}
+
+export async function sortCards() {
+  let category = getParam('category')
+  let data = (await getData(category)).filter(
+      (product) => product.Id !== "880RT" && product.Id !== "989CG");
+  let ele = document.querySelector('.product-list');
+  let selectBox = document.querySelector('#sort-by');
+  if (selectBox.value == 'alpha') {
+    data.sort(compare('NameWithoutBrand'));
+  }
+  else if (selectBox.value == 'price') {
+    data.sort(compare('FinalPrice'))
+  }
+  renderListWithTemplate(productCardTemplate, ele, data);
+}
+
+function compare(property) {
+   return function(product1, product2) { 
+    if (product1[property] < product2[property]) {
+    return -1;
+   }
+   else if (product1[property] > product2[property]) {
+    return 1;
+   } else { return 0;}
+  }
 }
