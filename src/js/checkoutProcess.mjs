776c0920a5d9a0484c0764796ac7e79984b547b8
alec willits <alecwillits@gmail.com>
2024-02-22 20:29:40 -0700

    started checkout process and finished form fields

---
+++ src/js/checkoutProcess.mjs
@@ -0,0 +1,30 @@
+import { getLocalStorage } from './utils.mjs';
+
+const checkoutProcess = {
+  key: '',
+  outputSelector: '',
+  list: [],
+  itemTotal: 0,
+  shipping: 0,
+  tax: 0,
+  orderTotal: 0,
+  init: function (key, outputSelector) {
+    this.key = key;
+    this.outputSelector = outputSelector;
+    this.list = getLocalStorage(key);
+    this.calculateItemSummary();
+  },
+
+  displayOrderTotals: function () {
+    const shipping = document.querySelector(this.outputSelector + ' #shipping');
+    const tax = document.querySelector(this.outputSelector + ' #tax');
+    const orderTotal = document.querySelector(
+      this.outputSelector + ' #orderTotal'
+    );
+    shipping.innerText = '$' + this.shipping;
+    tax.innerText = '$' + this.tax;
+    orderTotal.innerText = '$' + this.orderTotal;
+  },
+};
+
+export default checkoutProcess;
