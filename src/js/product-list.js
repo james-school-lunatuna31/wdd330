8f002e9beaa8c742c901ec3a90c01b55ccbba562
Lindsey Goode <ljgoode2012@gmail.com>
2024-02-24 21:13:20 -0500

    sort feature completed

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,5 +1,7 @@
 import { getParam, loadHeaderFooter } from './utils.mjs';
 import productList from './productList.mjs';
+import { sortCards } from './productList.mjs';
 
 productList(getParam('category'), '.product-list');
 loadHeaderFooter();
+document.querySelector('#sort-by').addEventListener('change', sortCards);
